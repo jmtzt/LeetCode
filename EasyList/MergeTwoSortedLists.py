e6c38ccc836be370167a8e57b0d4ca12a8b1284f
jmtzt <jmttozato@gmail.com>
2020-07-24 16:32:11 -0300

    problem3 solved

--- EasyList/MergeTwoSortedLists.py
+++ EasyList/MergeTwoSortedLists.py
@@ -5,4 +5,15 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode):
-        
+        dummy = ListNode(-1)
+        newList = dummy 
+        while (l1 != None and l2 != None):
+            if(l1.val <= l2.val):
+                newList.next = l1
+                l1 = l1.next
+            else:
+                newList.next = l2
+                l2 = l2.next
+            newList = newList.next
+        newList.next = l1 if l1 else l2
+        return dummy.next
